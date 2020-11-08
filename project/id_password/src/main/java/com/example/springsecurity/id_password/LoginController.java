6409daef691e63b425cceada757a3a8fd2032e83
reima4038 <reima4038@gmail.com>
2020-11-08 17:56:13 +0900

    Form認証(ID/PW)：ほぼBasic認証と同等の最低限のログイン機能を実装

---
+++ project/id_password/src/main/java/com/example/springsecurity/id_password/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.springsecurity.id_password;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // login.htmlを表示
+    }
+}
--- project/id_password/src/main/java/com/example/springsecurity/id_password/config/SecurityConfiguration.java
+++ project/id_password/src/main/java/com/example/springsecurity/id_password/config/SecurityConfiguration.java
@@ -10,6 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+        http.formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/top");
     }
 }
